18ec3cdf53deacfb0d2e0b51560cf6c683e0d8c7
romerodevelopments <gandresrp@gmail.com>
2024-04-08 21:16:54 -0400

    feat: add new endpoints on movie controller

--- src/main/java/com/example/moviesdbsrv/controller/MovieController.java
+++ src/main/java/com/example/moviesdbsrv/controller/MovieController.java
@@ -1,8 +1,12 @@
 package com.example.moviesdbsrv.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.moviesdbsrv.model.Movie;
@@ -26,5 +30,20 @@ public class MovieController {
   @GetMapping("/{id}")
   public Optional<Movie> getMovieById(@PathVariable Long id) {
     return movieService.getMovieById(id);
+  }
+
+  @PostMapping
+  public Movie createMovie(@RequestBody Movie movie) {
+    return movieService.createMovie(movie);
+  }
+
+  @PutMapping("/{id}")
+  public Movie updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
+    return movieService.updateMovie(id, movie);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteMovie(@PathVariable Long id) {
+    movieService.deleteMovie(id);
   }
 }
